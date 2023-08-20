ce297ead8a9ef4f4ea9327ad78e11e7e18f7d749
nadiaputriwahyudi <nadiaputriw123@gmail.com>
2023-08-20 13:31:24 +0700

    rubah judul

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pendaftaran</title>
+    <title>Pembukaan</title>
 </head>
 <body>
-    <h1>Pendaftaran</h1>
+    <h1>Pembukaan</h1>
 </body>
 </html>
